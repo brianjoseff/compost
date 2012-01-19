6c15056427be713f491beaaf70fcb36408a16bb2
Brian Joseff <Brian.D.P.Joseff@dartmouth.edu>
2012-01-19 00:54:01 -0500

    changed stripe keys to live

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
-Stripe.api_key = "1Km8QpFQOV3eErswXLOvaS6UgRIKH4Uc"
-STRIPE_PUBLIC_KEY = "pk_vWRJu7rE7SmniE08VPTbET4lDUS8f"
+Stripe.api_key = "NAExLd4KaYKZOgGwghofgp4h6oQb1yhT"
+STRIPE_PUBLIC_KEY = "pk_bbFEct0YyZO6Yx1a2zGyxmGbSyfRr "
